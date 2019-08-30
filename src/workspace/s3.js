fadd0fa4f2c851b735d76c90b45d5f9db8e488c6
Serhii Plyska <blysca@newtonideas.com>
2019-08-30 15:05:10 +0300

    Completed "Section 4: TypeScript and ES6"

---
+++ src/workspace/s3.js
@@ -0,0 +1,5 @@
+var s3myName = 'b9';
+var s3myAge = 33;
+var anything;
+anything = 12;
+// s3myName = 23;
---
+++ src/workspace/s3.ts
@@ -0,0 +1,7 @@
+let s3myName = 'b9';
+let s3myAge = 33;
+let anything;
+anything = 12;
+
+// s3myName = 23;
+

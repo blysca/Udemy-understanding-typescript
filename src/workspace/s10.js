f511831e1712b55b4d3843b060fca5f92f00282d
Serhii Plyska <blysca@newtonideas.com>
2019-09-02 15:43:39 +0300

    Completed "Section 10: Using JavaScript Libraries (like jQuery)"

---
+++ src/workspace/s10.js
@@ -0,0 +1,9 @@
+"use strict";
+// declare var $ : any;
+//77
+exports.__esModule = true;
+require("jQuery");
+$('button').on('click', function () {
+    alert('btn was clicked!');
+});
+$('#app').css({ 'background-color': 'green' });
---
+++ src/workspace/s10.ts
@@ -0,0 +1,9 @@
+// declare var $ : any;
+//77
+
+import 'jQuery';
+$('button').on('click',function () {
+  alert('btn was clicked!');
+});
+
+$('#app').css({'background-color': 'green'});

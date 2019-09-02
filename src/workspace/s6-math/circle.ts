cf3d98f9530d4cfb310e95dc6d0d5259b01fa066
Serhii Plyska <blysca@newtonideas.com>
2019-09-02 11:11:07 +0300

    Completed "Section 6: Namespaces and Modules"

---
+++ src/workspace/s6-math/circle.ts
@@ -0,0 +1,5 @@
+export const s6mPI = 3.14;
+
+export function calculateCircumference(diameter: number) {
+  return diameter * s6mPI;
+}
---
+++ src/workspace/s6-modules.ts
@@ -0,0 +1,7 @@
+// import {s6mPI, calculateCircumference} from  './s6-math/circle';
+import * as Circle from  './s6-math/circle';
+import rectangle from './s6-math/rectangle';
+
+console.log('*** PI = ', Circle.s6mPI);
+console.log('*** calculateCircumference = ', Circle.calculateCircumference(10));
+console.log('*** calculateRectangle =  ', rectangle(15, 12));
